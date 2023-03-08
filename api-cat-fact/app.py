6bd99c917f49f2031ed40a1f222ba894db80eb80
Jehad Cemanes <jehad.cemanes@gmail.com>
2023-03-08 11:52:57 +0800

    modify app.py

--- api-cat-fact/app.py
+++ api-cat-fact/app.py
@@ -2,15 +2,17 @@ from flask import Flask
 import requests
 import json
 
+# Define the URL of the Cat Fact API
 CAT_FACT_API_URL = 'https://cat-fact.herokuapp.com/'
 
 # Create a Flask app instance
 app = Flask(__name__)
 
 
-# Define the app route for the facts
+# Define a route to fetch facts from the Cat Fact API
 @app.route('/facts')
 def facts():
+    # Construct the API endpoint
     endpoint = CAT_FACT_API_URL + 'facts'
     # Make a request to the Cat-Fact API
     response = requests.get(endpoint)
